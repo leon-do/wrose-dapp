bec284e31b60ad0836b6815bc87ea18ce251d308
leon-do <leondo123@gmail.com>
2022-10-17 23:06:38 -0400

    maxWithdraw

--- components/MetaWithdraw.js
+++ components/MetaWithdraw.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Withdraw({ wrose }) {
-  const [amount, setAmount] = useState("0.09");
+  const [amount, setAmount] = useState("0.9");
   const [to, setTo] = useState("0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0");
 
   async function metaWithdraw() {
+    if (amount < 0.5) return alert(`Minimum amount must be: 0.5 WROSE`);
+    const maxWithdraw = await getMaxWithdraw();
+    if (maxWithdraw < 0.5) return alert(`Maximum amount must be: 0.5 WROSE`);
+    if (amount > maxWithdraw) return alert(`Maximum amount must be: ${maxWithdraw} WROSE`);
     const reward = 0.01 * amount;
     // fetch replay nonce
     const nonce = await fetch("/api/replayNonce", {
@@ -35,10 +39,25 @@ export default function Withdraw({ wrose }) {
     console.log(transaction);
   }
 
+  async function getMaxWithdraw() {
+    const address = await wrose.signerAddress();
+    const balanceOf = await fetch("/api/balanceOf", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        address,
+      }),
+    }).then((res) => res.json());
+    // account for the 1% fee
+    return Math.floor(balanceOf.response * 0.99 * 100) / 100;
+  }
+
   return (
     <>
       <div>
-        <input value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" />
         <input value={to} onChange={(e) => setTo(e.target.value)} />
         <button onClick={metaWithdraw}>Meta Withdraw</button>
       </div>
