7d499cc206b34647a44628fc4644e70fefb596c6
leon-do <leondo123@gmail.com>
2022-10-23 18:57:44 -0400

    comment out docs in nav

--- components/NavIndex.js
+++ components/NavIndex.js
@@ -12,9 +12,9 @@ export default function NavIndex() {
                 <Image width="32" height="32" className="h-8 w-auto lg:block" src="/images/oasis.svg" alt="WROSE" />
               </div>
               <div className="ml-6 flex space-x-8">
-                <Link href="#">
+                {/* <Link href="#">
                   <a className="inline-flex items-center border-b-2 hover:border-b-sky-500 px-1 pt-1 text-base font-medium">Docs</a>
-                </Link>
+                </Link> */}
                 <Link href="https://github.com/orgs/wrose-sapphire/repositories">
                   <a className="inline-flex items-center border-b-2 hover:border-b-sky-500 px-1 pt-1 text-base font-medium">Github</a>
                 </Link>
