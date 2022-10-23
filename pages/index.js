c95f015f9a77ac4008244059c875577fc928464f
leon-do <leondo123@gmail.com>
2022-10-23 11:55:04 -0400

    update css paratime column

--- pages/index.js
+++ pages/index.js
@@ -96,7 +96,7 @@ export default function Index() {
                   <table class="min-w-full divide-y divide-gray-300">
                     <thead class="bg-gray-50">
                       <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                           Paratime
                         </th>
                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
