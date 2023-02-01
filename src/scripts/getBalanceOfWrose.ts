eb6410391d0c0096fb774ac2c148558d29161106
leon-do <leondo123@gmail.com>
2023-01-31 22:31:48 -0500

    dynamic rpc

--- src/scripts/getBalanceOfWrose.ts
+++ src/scripts/getBalanceOfWrose.ts
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import WROSE from "./wrose";
 import * as sapphire from "@oasisprotocol/sapphire-paratime";
 
-const signer = sapphire.wrap(new ethers.Wallet("91d68008cc5323be3a777a568e78d5b2d043e76961fc892ff6353c56aacba145").connect(ethers.getDefaultProvider(sapphire.NETWORKS.testnet.defaultGateway)));
+const signer = sapphire.wrap(new ethers.Wallet("91d68008cc5323be3a777a568e78d5b2d043e76961fc892ff6353c56aacba145").connect(ethers.getDefaultProvider(process.env.CHAIN_RPC_URL)));
 const muleWose = new WROSE(signer);
 
 export default async function getBalanceOfWrose(wrose: WROSE) {
